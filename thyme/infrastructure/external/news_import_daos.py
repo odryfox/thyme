134a57529c1db50176d5dcab887451541fea6c84
Vyacheslav Rusov <odryfox@gmail.com>
2020-08-30 20:02:58 +0300

    Add newsapi docs link

--- thyme/infrastructure/external/news_import_daos.py
+++ thyme/infrastructure/external/news_import_daos.py
@@ -6,6 +6,9 @@ from domain.interfaces import IExternalNewsDAO
 
 
 class ExternalNewsAPIDAO(IExternalNewsDAO):
+    """
+    Docs: https://newsapi.org/docs/endpoints
+    """
     def __init__(self, api_key: str) -> None:
         self.api_key = api_key
         self.language = "ru"
